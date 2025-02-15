c0b728f19cc5f1c4cebe4f7c62125d7d5a1e8810
Connor Wheeler <Connorthevaliant@gmail.com>
2025-02-14 21:51:48 -0700

    Progress on Player movement

--- Game Jam 2/Assets/Scripts/FPSControllScript.cs
+++ Game Jam 2/Assets/Scripts/FPSControllScript.cs
@@ -5,18 +5,23 @@ using UnityEngine.InputSystem;
 public class FPSControllScript : MonoBehaviour
 {
     [SerializeField] Transform cam;
-    public float mouseSens = 2f;         //Mouse's sensitivity
-    private float mouseXV;          //
-    private float mouseYV;          //
-    private float camRotX = 0f;      //Current Rotation of the camera in the X axis
-    private float camRotY = 0f;      //Current Rotation of the camera in the Y axis
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody rigid; 
+    [SerializeField] float mouseSens = 2f;  //Mouse's sensitivity
+    private float mouseXV;                  //Movement of the mouse's X this frame
+    private float mouseYV;                  //Movement of the mouse's Y this frame
+    private float camRotX = 0f;             //Current Rotation of the camera in the X axis
+    private float camRotY = 0f;             //Current Rotation of the camera in the Y axis
+    [SerializeField] float moveSpeed = 1f;  //The players movementspeed
+    private float moveX;                    //Current input for movement in the X axis
+    private float moveZ;                    //Current input for movement in the Z axis
+    private Vector3 moveForce;                   //Current applied force for movement
+    
+    
+        void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        rigid = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,15 +30,21 @@ public class FPSControllScript : MonoBehaviour
         //Inputs
         mouseXV = Input.GetAxis("Mouse X") * mouseSens;
         mouseYV = Input.GetAxis("Mouse Y") * mouseSens;
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
 
-
-        //Camera handeling
+        //Camera Movement handeling
+        camRotX -= mouseXV;
         camRotY -= mouseYV;
         camRotY = Math.Clamp(camRotY, -90f, 90f);
 
         cam.localEulerAngles = Vector3.right * camRotY;
         transform.Rotate(Vector3.up * mouseXV);
+    }
 
-
+    private void FixedUpdate()
+    {
+        moveForce = cam.forward * moveX + cam.right * moveZ;
+        rigid.AddForce(moveForce.normalized * moveSpeed);
     }
 }
