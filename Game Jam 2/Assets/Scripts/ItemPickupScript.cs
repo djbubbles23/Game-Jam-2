d35f50307625cdc8e8c7c8f3c9e0c2b0dce404e9
Connor Wheeler <Connorthevaliant@gmail.com>
2025-02-15 15:26:17 -0700

    Pickup Item Shaders

--- Game Jam 2/Assets/Scripts/ItemPickupScript.cs
+++ Game Jam 2/Assets/Scripts/ItemPickupScript.cs
@@ -8,24 +8,28 @@ public class ItemPickupScript : MonoBehaviour
     [SerializeField] Transform lookDir; //Transfrom info for the camera
     [SerializeField] Transform PickupLoc; //Transform info for the location picked up objects will move to
     private float click; //Current input of the mouse left click
-    GameObject pickupObj;
+    GameObject lookObj; //Object the player is currently looking at
     
 
     void Update()
     {
         click = Input.GetAxis("Fire1");
+        RaycastHit hit;
+        if (Physics.Raycast (lookDir.position,lookDir.TransformDirection(Vector3.forward), out hit, 10)) {
+            if (lookObj != null && lookObj != hit.transform.gameObject && lookObj.tag == "Pickupable") {
+                lookObj.GetComponent<Outline>().OutlineWidth = 0;
+            }
+
+            if (hit.transform.gameObject.tag == "Pickupable") {
+                lookObj = hit.transform.gameObject;
+                lookObj.GetComponent<Outline>().OutlineWidth = 5;
+            }
+        }
     }
     void FixedUpdate()
     {
-        if (click != 0) {
-            RaycastHit hit;
-            if (Physics.Raycast (lookDir.position,lookDir.TransformDirection(Vector3.forward), out hit, 10)) {
-                Debug.Log(hit);
-                pickupObj = hit.transform.gameObject;
-                if (pickupObj.tag == "Pickupable") {
-                    Debug.Log("Pickupable");
-                }
-            }
+        if (click != 0 && lookObj != null && lookObj.tag == "Pickupable") {
+            Debug.Log("Pickup!!");
         }
     }
 }
